eccdc0c07f316850421beec3d1ba55e256b39521
vgTaranovsky <taranovscki.vadim@yandex.ru>
2023-07-17 16:07:12 +0400

    refactor

--- back/routers/note/types.ts
+++ back/routers/note/types.ts
@@ -1,11 +1,12 @@
+type Status = 'todo' | 'inprogress' | 'done';
+
 export type Note = {
   id: number;
   title: string;
+  status: Status;
   user_id: number;
   image: string | null;
-  status: 'todo' | 'inprogress' | 'done';
 };
 
-export type CreateDTO = Omit<Note, 'id' | 'user_id'>;
-
 export type PatchDTO = Pick<Note, 'status'>;
+export type CreateDTO = Omit<Note, 'id' | 'user_id'>;
